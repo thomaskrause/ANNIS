658aa676be667c4582f9bb0112900843fb410c2b
Thomas Krause <thomas@berlin-reloaded.de>
2013-01-28 15:21:15 +0100

    root calculation adjusted to corpus_ref change (welcome to the deja vue edition of ANNIS commit logs)

--- annis-service/src/main/distribution/sql/splitfacts/root.sql
+++ annis-service/src/main/distribution/sql/splitfacts/root.sql
@@ -3,12 +3,12 @@
 
 ALTER TABLE _rank ADD root boolean;
 
-CREATE INDEX _rank_corpus_parent_idx ON _rank (node_ref, parent);
+CREATE INDEX _rank_corpus_parent_idx ON _rank (corpus_ref, node_ref, parent);
 COMMIT;
 ANALYZE _rank;
 
 UPDATE _rank top SET root=
-(SELECT count(distinct _rank.parent) = 0 FROM _rank WHERE _rank.node_ref = top.node_ref);
+(SELECT count(distinct _rank.parent) = 0 FROM _rank WHERE _rank.node_ref = top.node_ref AND _rank.corpus_ref = top.corpus_ref);
 
 -- rank was changed, reanalyze it
 ANALYZE _rank(root);
