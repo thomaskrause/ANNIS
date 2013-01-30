cc2f5274f42576a825aee54a0338c46040779fae
Thomas Krause <thomas@berlin-reloaded.de>
2013-01-30 11:27:29 +0100

    functions should not return array

--- annis-service/src/main/distribution/sql/splitfacts/functions_get.sql
+++ annis-service/src/main/distribution/sql/splitfacts/functions_get.sql
@@ -1,29 +1,27 @@
 CREATE OR REPLACE FUNCTION getComponentType("type" char, namespace varchar, 
   "name" varchar, toplevel_corpus integer[]) 
 RETURNS smallint AS $f$
-SELECT ARRAY(
-  SELECT id 
-  FROM component_type 
-  WHERE 
-    "type" = $1 AND
-    namespace IS NOT DISTINCT FROM $2 AND
-    name IS NOT DISTINCT FROM $3 AND
-    toplevel_corpus = ANY($4)
-  LIMIT 1
-);
+SELECT id 
+FROM component_type 
+WHERE 
+  "type" = $1 AND
+  namespace IS NOT DISTINCT FROM $2 AND
+  name IS NOT DISTINCT FROM $3 AND
+  toplevel_corpus = ANY($4)
+LIMIT 1
+;
 $f$ LANGUAGE SQL IMMUTABLE;
 
 CREATE OR REPLACE FUNCTION getComponentTypeNameOnly("type" char, 
   "name" varchar, toplevel_corpus integer[]) 
 RETURNS smallint AS $f$
-SELECT ARRAY(
-  SELECT id 
-  FROM component_type 
-  WHERE 
-    "type" = $1 AND
-    name IS NOT DISTINCT FROM $2 AND
-    toplevel_corpus = ANY($3)
-  LIMIT 1
-);
+SELECT id 
+FROM component_type 
+WHERE 
+  "type" = $1 AND
+  name IS NOT DISTINCT FROM $2 AND
+  toplevel_corpus = ANY($3)
+LIMIT 1
+;
 $f$ LANGUAGE SQL IMMUTABLE;
 
